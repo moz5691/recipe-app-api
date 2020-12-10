615abe952531887ab69f07f0f91b58168c0369db
Chanho Ahn <moz5691@gmail.com>
2020-12-09 22:05:49 -0500

    Adding endpoint to user update

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'name')
+        fields = ('email', 'password', 'name',)
 
         extra_kwargs \
             = {'password': {'write_only': True, 'min_length': 5}}
@@ -16,6 +16,17 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it"""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
